36c24b11f2298946b60170e38e522ac757b345eb
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-04-07 19:36:18 +0200

    first commit, added ToDoInput component

---
+++ to-do-list-app/src/Components/TodoInput/TodoInput.js
@@ -0,0 +1,22 @@
+import React, { UseState } from "react";
+
+const TodoInput = (props) => {
+  const [inputValue, setInputValue] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.trim()) {
+      props.addTodo(inputValue);
+      setInputValue("");
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Enter a todo item"
+      />
+    </form>
+  );
+};
